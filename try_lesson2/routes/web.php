c3c8d0577a00f3073112ef2924b123c7eec1c5af
yusuf <yusuffront@gmail.com>
2021-09-14 18:55:44 +0800

    create new file models and routes and databases

--- try_lesson2/routes/web.php
+++ try_lesson2/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/posts', [PostController::class, 'index']);
+
+Route::get('/posts/{slug}', [PostController::class, 'view']);
