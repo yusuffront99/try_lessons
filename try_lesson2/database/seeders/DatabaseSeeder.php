51fb352e05b200ae66a8e73596bf436df1300939
yusuf <yusuffront@gmail.com>
2021-09-15 17:50:30 +0800

    fill database seeder

--- try_lesson2/database/seeders/DatabaseSeeder.php
+++ try_lesson2/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use App\Models\Category;
+use App\Models\post;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +17,52 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        Post::create([
+            "user_id"=>"1",
+            "category_id"=>"1",
+            "title"=>"Blog Pertama",
+            "slug"=>"blog_pertama",
+            "content"=>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum magnam sint tenetur rerum doloribus sunt voluptatum dolores, aut tempora quidem magni adipisci et recusandae incidunt cupiditate consectetur sit exercitationem vel aliquid eum assumenda placeat saepe? Totam quia, nihil laborum accusantium itaque veniam reiciendis dolores illum sequi quae incidunt, at quos odit pariatur. Quia, ducimus? Consectetur, culpa rem iure quasi minima neque odit voluptatibus fugit a incidunt aliquam itaque sapiente commodi temporibus ullam asperiores hic deserunt mollitia architecto veritatis eveniet maiores! Et officiis velit ex quisquam. Nihil officia fugiat ea, quae consequatur sit eum ut facere quo, suscipit aspernatur cupiditate quam!"
+        ]);
+
+        Post::create([
+            "user_id"=>"2",
+            "category_id"=>"2",
+            "title"=>"Blog Kedua",
+            "slug"=>"blog_kedua",
+            "content"=>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum magnam sint tenetur rerum doloribus sunt voluptatum dolores, aut tempora quidem magni adipisci et recusandae incidunt cupiditate consectetur sit exercitationem vel aliquid eum assumenda placeat saepe? Totam quia, nihil laborum accusantium itaque veniam reiciendis dolores illum sequi quae incidunt, at quos odit pariatur. Quia, ducimus? Consectetur, culpa rem iure quasi minima neque odit voluptatibus fugit a incidunt aliquam itaque sapiente commodi temporibus ullam asperiores hic deserunt mollitia architecto veritatis eveniet maiores! Et officiis velit ex quisquam. Nihil officia fugiat ea, quae consequatur sit eum ut facere quo, suscipit aspernatur cupiditate quam!"
+        ]);
+        
+        Post::create([
+            "user_id"=>"2",
+            "category_id"=>"1",
+            "title"=>"Blog Ketiga",
+            "slug"=>"blog_ketiga",
+            "content"=>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum magnam sint tenetur rerum doloribus sunt voluptatum dolores, aut tempora quidem magni adipisci et recusandae incidunt cupiditate consectetur sit exercitationem vel aliquid eum assumenda placeat saepe? Totam quia, nihil laborum accusantium itaque veniam reiciendis dolores illum sequi quae incidunt, at quos odit pariatur. Quia, ducimus? Consectetur, culpa rem iure quasi minima neque odit voluptatibus fugit a incidunt aliquam itaque sapiente commodi temporibus ullam asperiores hic deserunt mollitia architecto veritatis eveniet maiores! Et officiis velit ex quisquam. Nihil officia fugiat ea, quae consequatur sit eum ut facere quo, suscipit aspernatur cupiditate quam!"
+        ]);
+        
+        
+        Category::create([
+            "name"=>"Education",    
+            "slug"=>"education"
+        ]);
+        
+        Category::create([
+            "name"=>"Travelling",    
+            "slug"=>"travellig"
+        ]);
+        
+        User::create([
+            "name"=>"admin1",
+            "email"=>"admin123@gmail.com",
+            "password"=>bcrypt('12345')
+        ]);
+        
+        User::create([
+            "name"=>"admin2",
+            "email"=>"admin99@gmail.com",
+            "password"=>bcrypt('12345')
+        ]);        
     }
 }
